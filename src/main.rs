2e717ec9f9a28562390b6c48597d98a197d74629
MathieuD-QA <mathieu.dassie@protonmail.com>
2022-06-05 18:57:53 +0200

    seocnd commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use bevy::prelude::*;
 
+
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+struct Entity(u64);
+
+
+
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
-    App::new().run();
-    println!("hello world")
+    App::new()
+        .add_system(hello_world)
+        .run();
 }
